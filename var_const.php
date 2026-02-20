b3e9143c8da0e2c8a979861cf4a914dc10c20080
link33376 <rodrigo.developer376@gmail.com>
2026-02-20 17:08:13 -0300

    aula var e const

---
+++ var_const.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Const_var</title>
+</head>
+<body>
+    <?php 
+    
+    /*int = numero, 
+    string =  caractere, 
+    boleano = true/ false, 
+    float = 0.0 */ 
+
+    $aluno = "Arnoldo shuaseneguer";
+    $idade = "130 anos ";
+    
+    /*const inicia com a palavra "define"
+    entre aspas duplas 
+    1º constante 
+    2º valor */
+
+    
+    define("instituicao", "centro de educação tec. do amazonas");
+
+    echo "Olá aluno, $aluno $idade bem vindo ao " . instituicao
+
+
+
+    
+    
+    ?>
+    
+</body>
+</html>
