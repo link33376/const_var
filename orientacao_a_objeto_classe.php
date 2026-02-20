f48c40aa717b07849056ddaed7faf4ad10ce3736
link33376 <rodrigo.developer376@gmail.com>
2026-02-20 18:21:02 -0300

    Create orientacao_a_objeto_classe.php

---
+++ orientacao_a_objeto_classe.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Classes</title>
+</head>
+<body>
+<?php 
+
+class Carro {
+
+    public $marca;
+    public $modelo;
+    public $ano;
+    public $cor;
+    public $velocidade = 0;
+
+    public function acelerar(){
+        $this->velocidade += 10;
+        echo "O carro acelerou. Velocidade: " . $this->velocidade . " Km/h <br>";
+    }
+
+    public function frear(){
+        if($this->velocidade >= 10){
+            $this->velocidade -= 10;
+        }
+        echo "O carro freou. Velocidade: " . $this->velocidade . " Km/h <br>";
+    }
+}
+
+// Criando objeto
+$meuCarro = new Carro();
+
+// Chamando métodos
+$meuCarro->acelerar();
+$meuCarro->acelerar();
+$meuCarro->frear();
+
+?>
+</body>
+</html>
